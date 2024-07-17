a345baca58549d14033cc83810ba4c8da865564d
yuyiqi992 <13237181534@163.com>
2024-07-18 05:43:20 +0800

    test_allure
    Signed-off-by: yuyiqi992 <13237181534@163.com>

---
+++ test_allure/test_allure.py
@@ -0,0 +1,22 @@
+import allure
+import pytest
+
+
+@allure.epic("这是EPIC")
+@allure.feature("这是FEATURE")
+@allure.story("这是STORY")
+class TestAllure:
+    @allure.description("用例1的描述")
+    def test_normal_1(self):
+        print("OK1")
+
+    @allure.description("用例2的描述")
+    def test_normal_2(self):
+        print("OK2")
+
+    def test_normal_3(self):
+        print("OK3")
+
+
+if __name__ == '__main__':
+    pytest.main(['--alluredir=test_report'])
